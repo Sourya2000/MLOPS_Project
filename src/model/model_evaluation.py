e0f5c0106de01137beeecde6153f353604588006
Sourya Shome <48799896+Sourya2000@users.noreply.github.com>
2026-01-09 16:04:04 +0000

    added all the files

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -21,7 +21,7 @@ if not dagshub_token:
 os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
 os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-dagshub_url = "https://dagshub.com"
+dagshub_url ="https://dagshub.com"
 repo_owner = "Sourya2000"
 repo_name = "MLOPS_Project" 
 
